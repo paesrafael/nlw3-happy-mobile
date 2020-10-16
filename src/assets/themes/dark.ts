6f17d7200b0c9722df535742d31078cdeb68ec30
Rafael Paes <rfaelpaes@gmail.com>
2020-10-16 18:35:11 -0300

    chore: add themes inicial

---
+++ src/assets/themes/dark.ts
@@ -0,0 +1,13 @@
+export default {
+  title: 'dark',
+
+  colors: {
+    primary: '#15C3D6',
+    secondary: '',
+    white: '#FFFFFF',
+    whiteLight: 'rgba(255, 255, 255, 0.8)',
+    black: '#000000',
+    text: '#0089A5',
+    blueMediumLight: '#8FA7B3',
+  },
+}
