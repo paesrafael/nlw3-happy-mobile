4d4d5b2e8187a6eaa7e1d494adfdf348dd393153
Rafael Paes <rfaelpaes@gmail.com>
2020-10-17 21:09:03 -0300

    refactor: adiciona novas rotas

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,17 +1,26 @@
-import React from 'react'
+import React, { useContext } from 'react'
 
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
+
+import { ThemeContext } from 'styled-components/native'
 
 const { Navigator, Screen } = createStackNavigator()
 
 import OrphanagesMap from '~/pages/OrphanagesMap'
 import OrphanageDetails from '~/pages/OrphanageDetails'
 
+import SelectMapPosition from '~/pages/CreateOrphanages/SelectMapPosition'
+import OrphanageData from '~/pages/CreateOrphanages/OrphanageData'
+
+import Header from '~/components/Header'
+
 export default function Routes() {
+  const { colors } = useContext(ThemeContext)
+
   return (
     <NavigationContainer>
-      <Navigator screenOptions={{ headerShown: false }}>
+      <Navigator screenOptions={{ headerShown: false, cardStyle: { backgroundColor: colors.grayLight } }}>
         <Screen
           name="OrphanagesMap"
           component={OrphanagesMap}
@@ -20,6 +29,28 @@ export default function Routes() {
         <Screen
           name="OrphanageDetails"
           component={OrphanageDetails}
+          options={{
+            headerShown: true,
+            header: () => <Header showCancel={false} title="Orfanato" />
+          }}
+        />
+
+        <Screen
+          name="SelectMapPosition"
+          component={SelectMapPosition}
+          options={{
+            headerShown: true,
+            header: () => <Header title="Selecione no mapa" />
+          }}
+        />
+
+        <Screen
+          name="OrphanageData"
+          component={OrphanageData}
+          options={{
+            headerShown: true,
+            header: () => <Header title="Informe os dados" />
+          }}
         />
       </Navigator>
     </NavigationContainer>
